5d8d77ae6ebd29acc297cf9ccc3ebf3e62982d45
Mario Reder <mario.reder@pm.me>
2025-10-01 04:12:53 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets",
-  "VITE_CUSTOM_MENUS": "Spot, https://dexusdark.netlify.app/; Website, https://usdark.netlify.app/",
+  "VITE_CUSTOM_MENUS": "Spot, https://dex.usdark.app/; Website, https://usdark.app/",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
